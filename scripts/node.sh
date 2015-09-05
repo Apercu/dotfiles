85b26766a8586746d3375e610388fa7bb437be7e
meriadec <meriadec.pillet@gmail.com>
2015-09-05 04:33:34 +0200

    remove unused script

--- scripts/node.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir ~/local
-mkdir ~/node-latest-install
-cd ~/node-latest-install
-curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
-./configure --prefix=~/local
-make install
-curl https://www.npmjs.org/install.sh | sh
